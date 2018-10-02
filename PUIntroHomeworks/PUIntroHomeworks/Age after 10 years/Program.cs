d4ab990a1e4c42a80fdcc1055129061caa5e5043
Ivan Vasilev <battlemen12@gmail.com>
2018-10-02 16:03:22 +0300

    Included Homework t1&t2

---
+++ PUIntroHomeworks/PUIntroHomeworks/Age after 10 years/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Age_after_10_years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you ? ");
+            int AgeAfterTen = int.Parse(Console.ReadLine());
+            Console.WriteLine("You will be {0} after 10 years",AgeAfterTen + 10 );
+        }
+    }
+}
---
+++ PUIntroHomeworks/PUIntroHomeworks/TheUniqueNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheUniqueNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
